4536bd38fad75aec55a435458f39867f467392d3
Msokurenko@edu.riga.lv <you@example.com>
2023-12-13 10:19:25 +0200

    mes

--- calc.py
+++ calc.py
@@ -6,17 +6,19 @@ def megabit_to_megabyte(megabit):
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+   result = kilometers / litres
+   if kilometrs <100:
+       
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = (celsius*9/5)+32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
-    result = 0
+    result = text longth
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
